17363f01b84a2ee044e40472cc10dd977d7b2e71
NOOKX2 <thatchavit.t@ku.th>
2025-06-27 16:41:20 +0700

    add productGroup column

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -32,42 +32,44 @@ export default function Home() {
   }, [])
 
   return (
-    <div className='mt-[20px] mx-[20px]'>
+    <div className='my-[10px] mx-[10px] '>
       <table className='text-left'>
         <thead>
           <tr className='bg-red-400 border '>
-            <th className='p-3 border'>ชื่อร้าน</th>
-            <th className='p-3 border'>id</th>
-            <th className='p-3 border'>lat</th>
-            <th className='p-3 border'>lng</th>
-            <th className='p-3 border'>ที่อยู่</th>
-            <th className='p-3 border'>แขวง</th>
-            <th className='p-3 border'>เขต</th>
-            <th className='p-3 border'>จังหวัด</th>
-            <th className='p-3 border'>รหัสไปรษณีย์</th>
-            <th className='p-3 border'>สินค้า</th>
-            <th className='p-3 border'>Link</th>
+            <th className='p-1 border'>ชื่อร้าน</th>
+            <th className='p-1 border'>id</th>
+            <th className='p-1 border'>lat</th>
+            <th className='p-1 border'>lng</th>
+            <th className='p-1 border'>ที่อยู่</th>
+            <th className='p-1 border'>แขวง</th>
+            <th className='p-1 border'>เขต</th>
+            <th className='p-1 border'>จังหวัด</th>
+            <th className='p-1 border'>รหัสไปรษณีย์</th>
+            <th className='p-1 border'>สินค้า</th>
+            <th className='p-1 border'>Product Group</th>
+            <th className='p-1 border'>Link</th>
           </tr>
         </thead>
         <tbody>
 
           {stores.map((store) => (
             <tr className='bg-yellow-100' key={store.id}>
-              <td className='p-3 border'>{store.name}</td>
-              <td className='p-3 border'>{store.id}</td>
-              <td className='p-3 border'>{store.lat}</td>
-              <td className='p-3 border'>{store.lng}</td>
-              <td className='p-3 border'>{store.address}</td>
-              <td className='p-3 border'>{store.subdistrict}</td>
-              <td className='p-3 border'>{store.district}</td>
-              <td className='p-3 border'>{store.province}</td>
-              <td className='p-3 border'>{store.postcode}</td>
-              <td className='p-3 border'><ul>
+              <td className='p-1 border'>{store.name}</td>
+              <td className='p-1 border'>{store.id}</td>
+              <td className='p-1 border'>{store.lat}</td>
+              <td className='p-1 border'>{store.lng}</td>
+              <td className='p-1 border'>{store.address}</td>
+              <td className='p-1 border'>{store.subdistrict}</td>
+              <td className='p-1 border'>{store.district}</td>
+              <td className='p-1 border'>{store.province}</td>
+              <td className='p-1 border'>{store.postcode}</td>
+              <td className='p-1 border'><ul>
                 {store.products && store.products.map((product, index) => (
                   <li key={product.id}>{index+1}.{product.name}</li>
                 ))}
               </ul></td>
-              <td className='p-5 border'>
+              <td className='p-1 border'>{store.productGroup}</td>
+              <td className='p-1 border'>
                 <a className="text-blue-500" href={store.direction} target="_blank" rel="noopener noreferrer">แผนที่</a>
               </td>
             </tr>
